3562df4e27967d4beedb39b9e2a1f0b597205bf3
muhammadhamza2001 <muhammadhamzackt@gmail.com>
2024-10-25 15:07:51 +0500

    hero setion update

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -17,13 +17,13 @@ const Hero = () => {
             </video>
             <div className="overlay" /> 
             <Container className="content-container">
-                <Col xs={12} lg={6}>
+                <Col xs={12} lg={10} className='mx-auto text-center'>
                     <div className="hero pt-5">
                         <h1 className="text-white">BEST PLACE TO BUY AND SELL CRYPTO CURRENCY</h1>
                         <p className="text-white">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute  mollit anim id est laborum.
                         </p>
-                        <div className="d-flex gap-4 justify-content-center justify-content-lg-start">
+                        <div className="d-flex gap-4 justify-content-center">
                             <Button className="greenbtn">Join Presale</Button>
                             <Button className="transparentbtn">How To Buy</Button>
                         </div>
