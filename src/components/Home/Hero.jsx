836f55d6cb3613126f155f37c42c642c2d908216
muhammadhamza2001 <muhammadhamzackt@gmail.com>
2024-11-18 18:18:13 +0500

    Hero layout

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,34 +1,29 @@
 import React from 'react';
-import { Button, Col, Container } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import GlobeComponent from '../launchpad/Globe';
 
 const Hero = () => {
     return (
         <div className="heroouter">
-            <video
-                className="video-background" 
-                autoPlay
-                loop
-                muted
-                playsInline
-            >
-               
-                <source src={" https://res.cloudinary.com/dn0dm4fzf/video/upload/v1729689407/video.mp4"} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-            <div className="overlay" /> 
             <Container className="content-container">
-                <Col xs={12} lg={10} className='mx-auto text-center'>
-                    <div className="hero pt-5">
-                        <h1 className="text-white">The World’s First AI-Backed DeFi Asset Manager With Over $50M AUM </h1>
-                        <p className="text-white">
-                        Leverage the artificial intelligence advantage with the first DeFi launchpad identifying opportunities across the crypto market. Enjoy unprecedented gains and enhanced transparency. 
-                        </p>
-                        <div className="d-flex gap-4 justify-content-center">
-                            <Button className="greenbtn">Join Presale</Button>
-                            <Button className="transparentbtn">Whitepaper</Button>
+                <Row className='align-items-center'>
+                    <Col xs={12} lg={6} className=''>
+                        <div className="hero pt-5">
+                            <h1 className="text-white">The World’s First AI-Backed DeFi Asset Manager With Over <span>$50M AUM </span> </h1>
+                            <p className="text-white">
+                                Leverage the artificial intelligence advantage with the first DeFi launchpad identifying opportunities across the crypto market. Enjoy unprecedented gains and enhanced transparency.
+                            </p>
+                            <div className="d-flex gap-4">
+                                <Button className="greenbtn">Join Presale</Button>
+                                <Button className="transparentbtn">Whitepaper</Button>
+                            </div>
                         </div>
-                    </div>
-                </Col>
+                    </Col>
+                    <Col xs={12} lg={6}>
+                        <GlobeComponent/>
+                    </Col>
+                </Row>
+
             </Container>
         </div>
     );
