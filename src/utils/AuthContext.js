2bfc476d5c987f33fd56f9c4036511623df1cd69
alilogics007 <alilogics007@gmail.com>
2025-01-03 14:18:28 +0500

    auto logout add

--- src/utils/AuthContext.js
+++ src/utils/AuthContext.js
@@ -13,30 +13,52 @@ export const AuthProvider = ({ children }) => {
   const [isVerified, setIsVerified] = useState(false);
   const navigate = useRouter();
   const { disconnect } = useDisconnect();
+  const logoutAfterDays = process.env.NEXT_PUBLIC_LOGOUT_AFTER_DAYS || 7;
 
   const verifyAuth = () => {
     const verify = AuthVerify();
     setIsVerified(verify);
+
+    if (verify) {
+      const loginTimestamp = new Date().getTime();
+      localStorage.setItem("loginTimestamp", loginTimestamp);
+    }
   };
+
   const clearAllCookies = () => {
     const cookies = document.cookie.split("; ");
     cookies.forEach(cookie => {
       const [name] = cookie.split("=");
       document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
     });
-    // console.log("All cookies have been cleared!");
   };
 
   const logout = () => {
     localStorage.removeItem("user");
+    localStorage.removeItem("loginTimestamp");
     disconnect();
     setIsVerified(false);
-    clearAllCookies()
+    clearAllCookies();
     navigate.push("/");
   };
 
   useEffect(() => {
     verifyAuth();
+
+    const checkLogout = () => {
+      const loginTimestamp = localStorage.getItem("loginTimestamp");
+      if (loginTimestamp) {
+        const elapsedTime = new Date().getTime() - Number(loginTimestamp);
+        const logoutTime = logoutAfterDays * 24 * 60 * 60 * 1000;
+
+        if (elapsedTime >= logoutTime) {
+          logout();
+        }
+      }
+    };
+
+    const interval = setInterval(checkLogout, 60 * 60 * 1000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
